65cf8b4aa0ceca2e9f9878e3d82aebbf6eee7022
Jimena-04 <jimenagarcia0405.jg@gmail.com>
2025-10-16 10:08:52 -0600

    Boton para borrar un contaco del ArrayList y del Jlist

--- src/ada2/GUI_contactos.java
+++ src/ada2/GUI_contactos.java
@@ -3,6 +3,7 @@ package ada2;
 import java.awt.EventQueue;
 import java.util.ArrayList;
 
+import javax.swing.DefaultListModel;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -10,6 +11,12 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class GUI_contactos extends JFrame {
 
@@ -18,6 +25,8 @@ public class GUI_contactos extends JFrame {
 	ArrayList<Contactos> contactos = new ArrayList<>();
 	private JTextField Tf_nombre;
 	private JTextField Tf_tel;
+	DefaultListModel<String> JLNombres = new DefaultListModel<>();
+	
 
 	/**
 	 * Launch the application.
@@ -72,9 +81,44 @@ public class GUI_contactos extends JFrame {
 		lblNewLabel_2.setBounds(223, 129, 70, 19);
 		contentPane.add(lblNewLabel_2);
 		
+		JList list = new JList();
+		list.setBounds(441, 32, 139, 186);
+		contentPane.add(list);
+		list.setModel(JLNombres);
+		
 		Tf_tel = new JTextField();
 		Tf_tel.setBounds(297, 131, 109, 19);
 		contentPane.add(Tf_tel);
 		Tf_tel.setColumns(10);
+		
+		/*Boton para eliminar Realizado por Jimena; Se pregunta que contacto se va a eliminar y se guarda en una variable de tipo String, entra a un ciclo en el
+		 de tamaño del ArrayList, si el contacto existe se eliminar del ArrayList y de Jlist, le enviara un mensaje de que el contacto ya fue eliminado 
+		 y enseguida saldra del for, si el contacto no existe al salir del for se le enviara un mensaje de que no existe el contacto. 
+		 */
+		JButton btnBorrar = new JButton("Borrar");
+		btnBorrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String Nombre = JOptionPane.showInputDialog("¿Contacto a eliminar?");
+				boolean Res = false;
+			for (int i = 0; i<contactos.size(); i++) { 
+				
+				if(contactos.get(i).getNomb().equalsIgnoreCase(Nombre)) { 
+					contactos.remove(i);           
+					JLNombres.removeElementAt(i); 
+					JOptionPane.showMessageDialog(null,"El contacto " + Nombre + " fue eliminado");
+					Res = true; 
+					break;
+					
+				}
+			}
+			     if (Res = false) {
+			     JOptionPane.showMessageDialog(null,"El contacto " + Nombre + " no existe");
+			     }
+			}
+		});
+		btnBorrar.setBounds(482, 230, 89, 23);
+		contentPane.add(btnBorrar);
+		
+		
 	}
 }
